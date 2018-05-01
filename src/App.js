96ba04d947acf4ea249cf6849c71ff83b6821850
Viktor Masniy <vmasney@gmail.com>
2018-05-01 12:49:06 +0300

    adding basic components L93

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
-import './App.css';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
 
   render() {
-      return <p>test</p>
+      return (
+        <div>
+          <Layout>
+            <BurgerBuilder />
+          </Layout>
+        </div>
+      )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-//import {createStore} from 'redux';
-//import reducer from './reducers';
-
-//const store = createStore(reducer);
-
 const render = () => ReactDOM.render(
   <App />, document.getElementById('root'));
 
